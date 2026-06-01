1e47f64c90679878c8f0292fcf7b6915a54319b0
Vladimir <vladimir.firsov@solucionesinformaticasmagic.es>
2026-06-01 18:26:55 +0200

    Проверка изменений на Гит

--- app/src/main/java/com/pectinworld/intercom/MainActivity.java
+++ app/src/main/java/com/pectinworld/intercom/MainActivity.java
@@ -84,6 +84,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Проверка изменений на Гит.
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 100);
         }
